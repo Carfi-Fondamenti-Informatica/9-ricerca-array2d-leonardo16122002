83148c591ec61846a7e53ba86656f783733fee86
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-12-07 12:11:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+using namespace std;
+int main() {
+    char a[10][20];
+    char n[20];
+    int r;
+    for(int k=0;k<10;k++){
+        cin>>a[k];
+    }
+    cin>>n;
+
+    r=cerca(a,n);
+    if(r==-1){
+        cout<<"non presente"<<endl;
+    }else{
+        cout<<r<<endl;
+    }
+    /*for(int k=0;k<10;k++){
+        cout<<a[k]<<endl;
+    }*/
+
+    return 0;
 }
