c2d4ea3701718373fc6764f693a4186ec5d4b043
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-12-07 12:11:56 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by leona on 07/12/2021.
+//
+
+#ifndef ESERCIZIO_VALUTATO9_LIB_H
+#define ESERCIZIO_VALUTATO9_LIB_H
+int cerca(char[10][20],char[20]);
+#endif //ESERCIZIO_VALUTATO9_LIB_H
+
 
